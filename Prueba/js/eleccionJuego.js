0d44d07932ad280fbff73ac2bfaa8f15738236cd
Jose Maria Sierra Moreno <99356604+josusierra@users.noreply.github.com>
2024-05-08 14:29:44 +0200

    5.0
    actualizacion 5.0

--- Prueba/js/eleccionJuego.js
+++ Prueba/js/eleccionJuego.js
@@ -1,17 +1,10 @@
-// Función para verificar si el usuario es mayor de 18 años
-function verificarEdad() {
-    // Mostrar una alerta preguntando al usuario si tiene más de 18 años
-    var respuesta = confirm("¿Eres mayor de 18 años?");
-    
-    // Verificar la respuesta del usuario
-    if (respuesta) {
-        // Si el usuario confirma que es mayor de 18 años, no hacemos nada
-        alert("¡Bienvenido! Disfruta del sitio.");
+document.addEventListener("DOMContentLoaded", function(event) {
+    var respuesta = confirm("¿Tienes más de 18 años?");
+    if (respuesta == true) {
+        // Permitir acceso a la página
+        alert("Bienvenido a nuestra página.");
     } else {
-        // Si el usuario no es mayor de 18 años, redirigir a una búsqueda de gatos en Google
+        // Redirigir a buscar gatos en Google
         window.location.href = "https://www.google.com/search?q=gatos";
     }
-}
-
-// Llamar a la función para verificar la edad cuando se carga la página
-window.onload = verificarEdad;
+});
